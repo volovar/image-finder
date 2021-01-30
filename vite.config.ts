1d0ae2ebe9a1d9cd40af9099f1bc19e21d6acb5b
volovar <michaelvolovar@gmail.com>
2021-01-30 18:16:36 -0500

    feat(wip): updated search component
    Updated the search to use the service url

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,8 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [vue()],
-  server: {
-    proxy: {
-      "/api": {
-        target: mode === "development" ? "0.0.0.0:9010" : "",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
-    },
-  },
+
+  chokidarWatchOptions: {
+    usePolling: true
+  }
 }));
